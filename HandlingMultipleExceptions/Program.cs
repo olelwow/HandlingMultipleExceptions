83aaf06a9ed309da8b6b78b8c53186c0b8166d96
Olle Ristenstrand <olelwow@hotmail.com>
2024-11-27 11:07:39 +0100

    Add project files.

---
+++ HandlingMultipleExceptions/Program.cs
@@ -0,0 +1,22 @@
+﻿
+HandleMultipleExceptions("1", 1);
+
+static void HandleMultipleExceptions (string text, int index)
+{
+    int[] numbers = { 1, 2, 3 };
+    try
+    {
+        int integerFromString = Int32.Parse(text);
+        int valueAtIndex = numbers[index];
+        
+        Console.WriteLine(valueAtIndex);
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Invalid format");
+    }
+    catch (IndexOutOfRangeException)
+    {
+        Console.WriteLine("Index out of range");
+    }
+}
